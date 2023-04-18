e578a3dcfb15f243222ae0fa925cad74354d1b87
jbflores24 <jbflores24@hotmail.com>
2023-04-18 08:59:12 -0600

    mi primer commit 14042023

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php   include("includes/header_front.php");
+        include("includes/banner.php");
+?>
+    <div class="container mt-5">
+    
+<?php 
+    include("includes/articles.php");
+    include("includes/footer.php"); 
+?>
+       
